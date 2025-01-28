fb9ae6df584585f17f007d8aa0dffd03890f5890
THiago de sena <thiagosena316@gmail.com>
2025-01-28 16:41:02 -0300

    Selecionar tempo no gráfico

--- dataProcessor.py
+++ dataProcessor.py
@@ -6,24 +6,27 @@ class DataProcessor:
             "monitoramento/temperatura": None,
             "monitoramento/umidade": None
         }
-        self.previous_values = {
+        '''self.previous_values = {
             "temperatura": None,
             "umidade": None
-        }
+        }'''
 
     # Verifica o tópico e insere o valor no banco de dados
     def process_data(self,topic,payload):
         self.data_to_insert[topic]=payload  # Atribui o dicionário daquele tópico a sua mensagem
 
         if self.data_to_insert["monitoramento/temperatura"] is not None and \
-           self.data_to_insert["monitoramento/umidade"] is not None and \
-           (self.data_to_insert["monitoramento/temperatura"] != self.previous_values["temperatura"] or
-            self.data_to_insert["monitoramento/umidade"] != self.previous_values["umidade"]):
+           self.data_to_insert["monitoramento/umidade"] is not None:
             temperatura = self.data_to_insert["monitoramento/temperatura"]
             umidade = self.data_to_insert["monitoramento/umidade"]
             self.db.insert_into_database(temperatura, umidade)
-            self.previous_values["temperatura"] = temperatura
-            self.previous_values["umidade"] = umidade
+            '''self.previous_values["temperatura"] = temperatura
+            self.previous_values["umidade"] = umidade'''
+        
+        '''if topic in ["monitoramento/temperatura","monitoramento/umidade"]:
+            temperatura = payload if topic == "monitoramento/temperatura" else None
+            umidade = payload if topic == "monitoramento/umidade" else None
+            self.db.insert_into_database(temperatura,umidade)'''
 
         if topic == "monitoramento/heartbeat":
             print(payload)
--- graphGenerator.py
+++ graphGenerator.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from datetime import datetime, timedelta
 
 
 class GraphGenerator:
@@ -8,24 +9,42 @@ class GraphGenerator:
     def __init__(self,database):
         self.db = database
 
-    # Consulta o banco de dados para obter os últimos dados
+    # Consulta o banco de dados para obter todos dados
     @st.cache_data(ttl=30)  # Guarda os dados em um cache e Atualiza o cache a cada 30 segundos
-    def fetch_data(_self):
+    def fetch_all_data(_self):
         cursor = _self.db.conecao.cursor()  #Acesso o cursor do banco
-        cursor.execute("SELECT temperatura, umidade, data FROM valores ORDER BY data DESC LIMIT 500") # seleciona o 100 últimos valores de temperatura e data
+        cursor.execute("SELECT temperatura, umidade, data FROM valores ORDER BY data DESC") # seleciona o 100 últimos valores de temperatura e data
         data = cursor.fetchall()    #Coloco os valores selecionados na variável 'data'
-
         #Cria um DataFrame a partir dos resultados da consulta
         df = pd.DataFrame(data, columns=['temperatura','umidade','data'])
-        
-
         return df
 
+    # Consulta o banco de dados para obter os dados das últimas 24 horas
+    @st.cache_data(ttl=30)  # Guarda os dados em um cache e Atualiza o cache a cada 30 segundos
+    def fetch_data_for_last_n_days(_self,num_days):
+        end_data = datetime.now()
+        start_date = end_data - timedelta(days=num_days)
+        cursor = _self.db.conecao.cursor()  #Acesso o cursor do banco
+        query = f"SELECT temperatura, umidade, data FROM valores WHERE data >= '{start_date:%Y-%m-%d %H:%M:%S}' ORDER BY data DESC" #Precisa usar o f antes para saber que ali dentro terá uma variável
+        cursor.execute(query) # seleciona o 100 últimos valores de temperatura e data
+        data = cursor.fetchall()    #Coloco os valores selecionados na variável 'data'
+        #Cria um DataFrame a partir dos resultados da consulta
+        df = pd.DataFrame(data, columns=['temperatura','umidade','data'])
+        return df
+
+    @st.cache_data(ttl=30) 
+    def fetch_data_start_and_end(_self,start_date,end_date):
+        cursor = _self.db.conecao.cursor()  #Acesso o cursor do banco
+        query = f"SELECT temperatura, umidade, data FROM valores WHERE data BETWEEN '{start_date}' AND '{end_date}' ORDER BY data DESC" #Precisa usar o f antes para saber que ali dentro terá uma variável
+        print(query)
+        cursor.execute(query) # seleciona o 100 últimos valores de temperatura e data
+        data = cursor.fetchall()    #Coloco os valores selecionados na variável 'data'
+        #Cria um DataFrame a partir dos resultados da consulta
+        df = pd.DataFrame(data, columns=['temperatura','umidade','data'])
+        return df
 
     # Cria o gráfico
     def create_graph(self,data):
-
-        st.title("Gráfico de Temperatura")
 
         fig = px.line(data,x='data',y=['temperatura','umidade'],title='Evolução da temperatura e Umidade')
         fig.update_traces(mode="markers+lines",hovertemplate=None) # Altera a visualização das informações no texto do mouser hover do gráfico
@@ -44,8 +63,42 @@ class GraphGenerator:
     
     # Atualiza o gráfico com os valores novos
     def update_graph(self):
-        df = self.fetch_data()  #Pega os valores do dataframe criado na consulta do banco
-        self.create_graph(df)
+        st.title("Gráfico de Monitoramento")
+
+        col1,col2,col3,col4,col5= st.columns(5,gap="small",vertical_alignment="center")
+        df = self.fetch_all_data() #Por padrão já mostra todos os dados
+        #Botões para a seleção do intevalo
+        with col1:
+            if st.button('Todos os dados'):
+                df = self.fetch_all_data()  #Pega os valores do dataframe criado na consulta do banco
+                
+        with col2:
+            if st.button('1 Dia'):
+                df = self.fetch_data_for_last_n_days(1)
+
+        with col3:
+            if st.button('7 Dias'):
+                df = self.fetch_data_for_last_n_days(7)
+
+        with col4:
+            if st.button('30 Dias'):
+                df = self.fetch_data_for_last_n_days(30)
+
+        with st.expander('Selecionar Intervalo de Datas',icon=":material/search:"):
+            col1,col2 = st.columns(2)
+            with col1:
+                start_date = st.date_input("Data Inicial",datetime.today(),key="start")
+            with col2:
+                end_date = st.date_input("Data Final",datetime.today(),key="end")
+            if st.button('Buscar'):
+                if start_date and end_date:
+                    df = self.fetch_data_start_and_end(start_date,end_date)
+                else:
+                    st.warning('Selecione a data de inicil e fim da busca!',icon=":material/warning:")
+        
+        
+        if df is not None:
+            self.create_graph(df)  
 
     '''def create_graph(self,data):
         if 'fig' not in st.session_state:
